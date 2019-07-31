b806eeb844675ef432b1253f976ceced18a3357d
Karel Hala <khala@redhat.com>
2019-07-31 10:45:22 +0200

    Add download action to toolbar (#5)

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,2 +1,3 @@
 export const LOAD_ALL = 'LOAD_ALL';
 export const LOAD_ONE_API = 'LOAD_ONE_API';
+export const SELECT_ROW = 'SELECT_ROW';
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,7 @@
 import { applyReducerHash } from '@redhat-cloud-services/frontend-components-utilities/files/ReducerRegistry';
 import * as ACTIONS from './actionTypes';
 
-const defaultState = { loaded: false };
+const defaultState = { loaded: false, selectedRows: {}};
 const disabledApis = [ '/api/webhooks', '/api/aiops-insights-clustering' ];
 
 function dataLoaded(state, { payload }) {
@@ -27,9 +27,34 @@ function detailLoaded(state, { payload: { latest, ...payload }}) {
     };
 }
 
+function onSelectRow(state, { payload: { isSelected, row }}) {
+    const selectedRows = {
+        ...state.selectedRows || {},
+        ...Array.isArray(row) ? row.reduce((acc, curr) => ({
+            ...acc,
+            [curr.cells[0].value]: {
+                isSelected,
+                appName: curr.cells[0].value,
+                version: curr.cells[2].value
+            }
+        }), {}) : {
+            [row.cells[0].value]: {
+                isSelected,
+                appName: row.cells[0].value,
+                version: row.cells[2].value
+            }
+        }
+    };
+    return {
+        ...state,
+        selectedRows
+    };
+}
+
 export const services = applyReducerHash({
     [`${ACTIONS.LOAD_ALL}_FULFILLED`]: dataLoaded,
-    [`${ACTIONS.LOAD_ALL}_PENDING`]: () => ({ loaded: false })
+    [`${ACTIONS.LOAD_ALL}_PENDING`]: () => defaultState,
+    [ACTIONS.SELECT_ROW]: onSelectRow
 }, defaultState);
 
 export const detail = applyReducerHash({
